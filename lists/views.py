ad4cb010ce688742625f3f7a1e33e462d56079d8
liyande <liyande>
2017-05-22 20:05:25 +0800

    20170522

---
+++ lists/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+# from django.http import HttpResponse
+#
+# def home_page(request):
+#     return HttpResponse('<html><title>To-Do lists</title></html>')
+
+def home_page(request):
+    return render(request,"home.html")
